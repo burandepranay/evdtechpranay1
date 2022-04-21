b43878f173da186bfc904872942f4f04a9a8c08a
burandepranay <pranayburande1@gmail.com>
2022-04-21 22:03:55 +0530

    task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './auth.guard';
+import { Auth1Guard } from './auth1.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"register",component: RegisterComponent, canActivate : [AuthGuard]
+  },
+  {
+    path:"login",component:LoginComponent, canActivate : [AuthGuard]
+  },
+  {
+    path:"dashboard",component:DashboardComponent, canActivate : [Auth1Guard]
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
